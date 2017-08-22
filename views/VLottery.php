03280ca94f5aca69d5e291f0a6996fa0ecc573ef
daniil_panov <daniil_panov2005@mail.ru>
2017-08-22 14:41:09 +0300

    Лотерея готова

--- views/VLottery.php
+++ views/VLottery.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <link href="style/style.css" rel="stylesheet" />
+</head>
+
+<body >
 <?php
 function __autoload($class_name)
 {
@@ -6,25 +14,27 @@ function __autoload($class_name)
 if(!$_POST) {
     ?>
 
-    <h1>ЛОТТЕРЕЯ!</h1>
-    <h3>6 из 36</h3>
+<table>
+    <tr>
+    <td><h1>ЛОТТЕРЕЯ!</h1></td>
+    </tr>
+    <tr>
+    <td><h3>6 из 36</h3></td>
+    </tr>
     <form method="post">
         ведите 6 любых чисел от 1 до 36
-        1-е <input type="text" name="numb1">
-        2-е <input type="text" name="numb2">
-        3-е <input type="text" name="numb3">
-        4-е <input type="text" name="numb4">
-        5-е <input type="text" name="numb5">
-        6-е <input type="text" name="numb6">
-        <input type="submit" value="продолжить">
-    </form>
-
-    <?php
-
-}
-else{
-
+        <tr>
+            <td>1-е <input type="text" name="numb1"></td>
+            <td>2-е <input type="text" name="numb2"></td>
+            <td>3-е <input type="text" name="numb3"></td>
+            <td>4-е <input type="text" name="numb4"></td>
+            <td>5-е <input type="text" name="numb5"></td>
+            <td>6-е <input type="text" name="numb6"></td>
+        </tr>
+        <?php
     $numb1= new CLottery($_POST);
     $numb1->checklottery();
 }
 ?>
+</body>
+</html>
