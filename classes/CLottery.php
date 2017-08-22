1efcb07047c70c4cf8162d2205e5059283e91f8f
daniil_panov <daniil_panov2005@mail.ru>
2017-08-22 11:44:05 +0300

    Лотерея готова

--- classes/CLottery.php
+++ classes/CLottery.php
@@ -1,5 +1,5 @@
-<?php
 
+<?php
 class CLottery
 {
     public $one;
@@ -8,7 +8,6 @@ class CLottery
     public $four;
     public $five;
     public $six;
-
     public function __construct($data)
     {
         $this->one=$data["numb1"];
@@ -18,32 +17,50 @@ class CLottery
         $this->five=$data["numb5"];
         $this->six=$data["numb6"];
     }
-
     public function checklottery(){
         $a=rand(1,36);
         for($b=rand(1,36);$b==$a;){
             $b=rand(1,36);
         }
-        echo "a = ".$a;
-        echo "a = ".$b;
+        for($c=rand(1,36);$c==$b || $c==$a;){
+            $c=rand(1,36);
+        }
+        for($d=rand(1,36);$d==$c || $d==$b || $d==$a;){
+            $d=rand(1,36);
+        }
+        for($e=rand(1,36);$e==$d || $e==$c || $e==$b || $e==$a;){
+            $e=rand(1,36);
+        }
+        for($f=rand(1,36);$f==$e || $f==$d || $f==$c || $f==$b || $f==$a;){
+            $f=rand(1,36);
+        }
+        if ($_POST) {
+
+            ?>
+            <table>
+               <tr>
+                   <td>$one</td>
+                   <td>$two</td>
+                   <td>$three</td>
+                   <td>$four</td>
+                   <td>$five</td>
+                   <td>$six</td>
+               </tr>
+                <tr>
+                    <td>$a</td>
+                    <td>$b</td>
+                    <td>$c</td>
+                    <td>$d</td>
+                    <td>$e</td>
+                    <td>$f</td>
+                </tr>
+            </table>
+            <?php
+        }
     }
 }
-
-
-for($c=rand(1,36);$c==$b || $c==$a;){
-    $c=rand(1,36);
-}
-for($d=rand(1,36);$d==$c || $d==$b || $d==$a;){
-    $d=rand(1,36);
-}
-for($e=rand(1,36);$e==$d || $e==$c || $e==$b || $e==$a;){
-    $e=rand(1,36);
-}
-for($f=rand(1,36);$f==$e || $f==$d || $f==$c || $f==$b || $f==$a;){
-    $f=rand(1,36);
-}
-if ($a==$one1 && $b==$two2 && $c==$three3 && $d==$four4 && $e==$five5 && $f==$six6){
-   echo "ВЫ ВЫИГРАЛИ!";
+if ($a==$one && $b==$two && $c==$three && $d==$four && $e==$five && $f==$six){
+    echo "ВЫ ВЫИГРАЛИ!";
 }
 else{
     echo "Вы проиграли. Попробуете сыграть ещё?";
