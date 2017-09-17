3fb75de8fb2e941a06bcb4de7db73e652dbaa40d
daniil_panov <daniil_panov2005@mail.ru>
2017-09-17 22:12:14 +0300

    добавил цикл for

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 
 <head>
-    <link href="style/style.css" rel="stylesheet" />
+    <link href="../style/victorina.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 
--- views/VVictorina.php
+++ views/VVictorina.php
@@ -8,29 +8,31 @@ function __autoload($class_name)
 // подключимся к БД
 $connectDB = new Db();
 $answers = new CVictorina();
-$data = $answers->getoneAnswer(1);
-foreach ($data as $value)
-{
-    //$answers[] = $value;
-}
-if($_GET){
-    $user_responce = $_GET['response'];
-    if($user_responce == $value['valid_responce']){
-        echo "Правильный ответ";
+for ($id = 1;$id<=99;$id++){
+    $data = $answers->getoneAnswer($id);
+    foreach ($data as $value)
+    if($_GET){
+        $user_responce = $_GET['response'];
+        if($user_responce == $value['valid_responce']){
+            echo "Правильный ответ";
+        }
+        else{
+            echo "Вы проиграли";
+        }
     }
-    else{
-        echo "Вы проиграли";
-    }
+    ?>
+    <div class="jumbotron">
+        <?="<center><p>".$value['question']."</p></center>";?>
+    </div>
+
+    <center><a class="btn btn-primary btn-lg" href="?response=<?=$value['response1'];?>" role="button"><?=$value['response1'];?></a>
+        <a class="btn btn-primary btn-lg" href="?response=<?=$value['response2'];?>" role="button"><?=$value['response2'];?></a>
+        <a class="btn btn-primary btn-lg" href="?response=<?=$value['response3'];?>" role="button"><?=$value['response3'];?></a>
+        <a class="btn btn-primary btn-lg" href="?response=<?=$value['response4'];?>" role="button"><?=$value['response4'];?></a></center>
+    <?php
+    //сделать что-то типо этого (см. далее).
+    /*if ($value == $value){
+        die();
+    }*/
 }
-?>
-<div class="jumbotron">
-    <?="<p>".$value['question']."</p>";?>
-</div>
-
-<a class="btn btn-primary btn-lg" href="?response=<?=$value['response1'];?>" role="button"><?=$value['response1'];?></a>
-<a class="btn btn-primary btn-lg" href="?response=<?=$value['response2'];?>" role="button"><?=$value['response2'];?></a>
-<a class="btn btn-primary btn-lg" href="?response=<?=$value['response3'];?>" role="button"><?=$value['response3'];?></a>
-<a class="btn btn-primary btn-lg" href="?response=<?=$value['response4'];?>" role="button"><?=$value['response4'];?></a>
-
-<?php
 require_once "../footer.php";
