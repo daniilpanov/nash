a6ec28256b3c3a6d78ab45660fb54d2d39d86c88
ilya682003 <ilyanfs682003@mail.ru>
2017-09-17 11:56:05 +0300

    Викторина (добавили кнопки и проверку правильного ответа)

--- views/VVictorina.php
+++ views/VVictorina.php
@@ -1,5 +1,6 @@
 
 <?php
+require_once "../header.php";
 function __autoload($class_name)
 {
 require_once("../classes/".$class_name . ".php");
@@ -16,15 +17,25 @@ foreach ($data as $value)
        //$answers[] = $value;
 }
 
-echo "<PRE>";
-var_export($value);
-echo "</PRE>";
-// Надо вставить в кнопки ответы.
+if($_GET){
+    $user_responce = $_GET['response'];
+    if($user_responce == $value['valid_responce']){
+        echo "Правильный ответ";
+    }
+    else{
+        echo "Вы проиграли";
+    }
+}
+
 ?>
-<form method="post">
-    <!--question-->
-    <button><!--(response1)--></button>
-    <button><!--(response2)--></button>
-    <button><!--(response3)--></button>
-    <button><!--(response4)--></button>
-</form>
+<div class="jumbotron">
+    <?="<p>".$value['question']."</p>";?>
+</div>
+
+    <a class="btn btn-primary btn-lg" href="?response=<?=$value['response1'];?>" role="button"><?=$value['response1'];?></a>
+    <a class="btn btn-primary btn-lg" href="?response=<?=$value['response2'];?>" role="button"><?=$value['response2'];?></a>
+    <a class="btn btn-primary btn-lg" href="?response=<?=$value['response3'];?>" role="button"><?=$value['response3'];?></a>
+    <a class="btn btn-primary btn-lg" href="?response=<?=$value['response4'];?>" role="button"><?=$value['response4'];?></a>
+
+<?php
+require_once "../footer.php";
