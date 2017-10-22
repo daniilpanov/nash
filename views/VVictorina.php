8218414b107e5097fa7822fb09ba0066c0c33194
daniil_panov <daniil_panov2005@mail.ru>
2017-10-22 19:33:52 +0300

    ни одна версия викторины не работает(наверное, сбой в XAMPP.)

--- views/VVictorina.php
+++ views/VVictorina.php
@@ -1,5 +1,8 @@
 
 <?php
+// У меня почему-то не работает это.
+# Даже то, что было на GitHub, хотя раньше - работало.
+
 require_once "../header.php";
 function __autoload($class_name)
 {
@@ -9,38 +12,37 @@ function __autoload($class_name)
 $connectDB = new Db();
 $answers = new CVictorina();
 
-$good =1;
-
-for($id=1;$id<=$good;$id++)
+for ($id=1;$id>=99;)
 {
     $data = $answers->getoneAnswer($id);
     foreach ($data as $value)
+
+
     if($_GET){
         $user_responce = $_GET['response'];
         if($user_responce == $value['valid_responce']){
-            $answ = "Правильный ответ";
-            $good++;
-            /*?>
-            <a class ="btn btn-primary btn-lg" href="?then <?=$user?>" role="button"><h3>Далее</h3></a>
-
-            <?php
-            if ($user !=  $i){
-                die();
+            function id($id){
+                $id++;
             }
-*/
-        }
-        else/*if($user_responce != $user)*/{
-            $answ = "Вы проиграли";
-        }
-        /*else{
-            $answ = "Правильный ответ";
+            ?>
+            <script>
+                setTimeout(function id,500,$id)
+
+                )
+            </script>
+            <?php
+            for ($numb = 5;$numb<=0;$numb--)
+
+            $answ = "Правильный ответ! Через ".$numb."Вы перейдёте к следующему заданию.";
+            if ($numb == 0){
+                $answ ="";
+            }
         }
 
-         */
     }
 
 }
-require_once "../footer.php";
+
 ?>
 <h1><?=$answ;?></h1>
 <div class="jumbotron">
@@ -52,3 +54,5 @@ require_once "../footer.php";
 <a class="btn btn-primary btn-lg" href="?response=<?=$value['response2'];?>" role="button"><?=$value['response2'];?></a>
 <a class="btn btn-primary btn-lg" href="?response=<?=$value['response3'];?>" role="button"><?=$value['response3'];?></a>
 <a class="btn btn-primary btn-lg" href="?response=<?=$value['response4'];?>" role="button"><?=$value['response4'];?></a>
+<?php
+require_once "../footer.php";
