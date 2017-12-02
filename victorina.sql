5cfbd0b0d1ba6a0ab5b031bb07ec31d9dc1e6f09
ilya682003 <ilyanfs682003@mail.ru>
2017-12-02 14:53:28 +0300

    Добавили дамп БД

--- victorina.sql
+++ victorina.sql
@@ -1,6 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Дек 02 2017 г., 12:45
+-- Версия сервера: 10.1.16-MariaDB
+-- Версия PHP: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `victorina`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `answers`
+--
 
 CREATE TABLE `answers` (
   `id` int(3) NOT NULL,
@@ -12,12 +36,34 @@ CREATE TABLE `answers` (
   `valid_responce` text NOT NULL COMMENT 'правильный ответ'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Ответы к викторине';
 
+--
+-- Дамп данных таблицы `answers`
+--
+
 INSERT INTO `answers` (`id`, `question`, `response1`, `response2`, `response3`, `response4`, `valid_responce`) VALUES
 (1, 'Какого цвета солнце?', 'оранжевое', 'красное', 'коричневое', 'желтое ', 'желтое'),
-(2, 'Сколько месяцев в году?', '5', '4', '10', '12', '12');
+(2, 'Сколько месяцев в году?', '5', '4', '10', '12', '12'),
+(3, 'Кто такой колобок', 'сказочный герой', 'сосед', 'друг', 'президент', 'сказочный герой');
 
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `answers`
+--
 ALTER TABLE `answers`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `answers`
+--
 ALTER TABLE `answers`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
