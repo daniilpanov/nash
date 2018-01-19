3a596cca0ec3dc2589667c55246e880b2e79f485
daniil_panov <daniil_panov2005@mail.ru>
2018-01-19 21:35:28 +0300

    в чём ошибка?

--- admin/classes/CVictorina.php
+++ admin/classes/CVictorina.php
@@ -5,18 +5,62 @@ class CVictorina extends MVicrorina
     public function doing()
     {
         // Проверка запроса пользователя
-        if ($_GET['level'] === 'new')
+        if ($_GET['level'])
         {
-            $answer = $this->NewAnswer($_POST);
-            return $answer;
-        }
-        elseif ($_GET['level'] === 'add')
-        {
-            $answer = $this->AddAnswer($_POST);
-        }
-        elseif ($_GET['level'] === 'delete')
-        {
-            $answer = $this->DeleteAnswer($_POST);
+            if ($_GET['level'] == 'new')
+            {
+                ?>
+                <form method="post">
+                    <input type="text" name="question" placeholder="ВОПРОС">
+                    <input type="text" name="resp1" placeholder="ОТВЕТ №1">
+                    <input type="text" name="resp2" placeholder="ОТВЕТ №2">
+                    <input type="text" name="resp3" placeholder="ОТВЕТ №3">
+                    <input type="text" name="resp4" placeholder="ОТВЕТ №4">
+                    <input type="text" name="v_resp" placeholder="ПРАВИЛЬНЫЙ ОТВЕТ" style="background-color: darkblue">
+                    <input type="submit" name="go" value="Отправить">
+                </form>
+                <?php
+                if($_POST['go'])
+                {
+                    $answer = $this->NewAnswer($_POST, $_GET);
+                    return $answer;
+                }
+
+            }
+            elseif ($_GET['level'] == 'add')
+            {
+                ?>
+                <form method="post">
+                    <input type="text" name="key" placeholder="ID">
+                    <input type="text" name="question" placeholder="ВОПРОС">
+                    <input type="text" name="resp1" placeholder="ОТВЕТ №1">
+                    <input type="text" name="resp2" placeholder="ОТВЕТ №2">
+                    <input type="text" name="resp3" placeholder="ОТВЕТ №3">
+                    <input type="text" name="resp4" placeholder="ОТВЕТ №4">
+                    <input type="text" name="v_resp" placeholder="ПРАВИЛЬНЫЙ ОТВЕТ" style="background-color: darkblue">
+                    <input type="submit" name="go" value="Отправить">
+                </form>
+                <?php
+                if ($_POST['go'])
+                {
+                    $answer = $this->AddAnswer($_POST, $_GET);
+                    return $answer;
+                }
+            }
+            elseif ($_GET['level'] == 'delete')
+            {
+                ?>
+                <form method="post">
+                    <input type="text" name="question" placeholder="НОМЕР СТРОКИ">
+                    <input type="submit" name="go" value="Отправить">
+                </form>
+                <?php
+                if ($_POST['go'])
+                {
+                    $answer = $this->DeleteAnswer($_GET, $_POST);
+                    return $answer;
+                }
+            }
         }
     }
 }
--- admin/views/VVictorina.php
+++ admin/views/VVictorina.php
@@ -11,54 +11,5 @@
 $z = new CVictorina();
 if ($_GET['level'])
 {
-    if ($_GET['level'] == 'new')
-    {
-        ?>
-        <form method="post">
-            <input type="text" name="question" placeholder="ВОПРОС">
-            <input type="text" name="resp1" placeholder="ОТВЕТ №1">
-            <input type="text" name="resp2" placeholder="ОТВЕТ №2">
-            <input type="text" name="resp3" placeholder="ОТВЕТ №3">
-            <input type="text" name="resp4" placeholder="ОТВЕТ №4">
-            <input type="text" name="v_resp" placeholder="ПРАВИЛЬНЫЙ ОТВЕТ" style="background-color: darkblue">
-            <input type="submit" name="go" value="Отправить">
-        </form>
-        <?php
-        if($_POST['go'])
-        {
-            $data = $answer->doing($_POST, $_GET);
-        }
-
-    }
-    elseif ($_GET['level'] == 'add')
-    {
-        ?>
-        <form method="post">
-            <input type="text" name="question" placeholder="ВОПРОС">
-            <input type="text" name="resp1" placeholder="ОТВЕТ №1">
-            <input type="text" name="resp2" placeholder="ОТВЕТ №2">
-            <input type="text" name="resp3" placeholder="ОТВЕТ №3">
-            <input type="text" name="resp4" placeholder="ОТВЕТ №4">
-            <input type="text" name="v_resp" placeholder="ПРАВИЛЬНЫЙ ОТВЕТ" style="background-color: darkblue">
-            <input type="submit" name="go" value="Отправить">
-        </form>
-        <?php
-        if ($_POST['go'])
-        {
-            $data = $answer->doing();
-        }
-    }
-    elseif ($_GET['level'] == 'delete')
-    {
-        ?>
-        <form method="post">
-            <input type="text" name="question" placeholder="НОМЕР СТРОКИ">
-            <input type="submit" name="go" value="Отправить">
-        </form>
-        <?php
-        if ($_POST['go'])
-        {
-            $data = $answer->doing($_GET, $_POST);
-        }
-    }
+    $data = $answer->doing($_GET['level']);
 }
